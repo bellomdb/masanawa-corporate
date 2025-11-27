1c722df197064d1364164c0f4b7ce80956c355ec
Bello Abu Atiku <you@example.com>
2025-11-27 09:32:23 +0100

    chore: remove .vercel from git

--- .vercel/output/_functions/chunks/supabase_DdrpAs7p.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://uxzvyjnpiobpzmtqmuyz.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4enZ5am5waW9icHptdHFtdXl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwOTM3NjksImV4cCI6MjA3OTY2OTc2OX0.ELnmy-tjpRWPKxq7yyfU18bXGyImu-h6yE9U9JiXRjk";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase as s };
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_C9qafe8F.mjs';
-export { renderers } from '../renderers.mjs';
